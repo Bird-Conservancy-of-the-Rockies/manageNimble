c9e9faf1936dde43d5b35a3d1af3774d587ac2db
Quresh Latif <qureshlatif@yahoo.com>
2025-04-11 11:40:52 -0600

    trying to improve RAM and CPU usage
    - Separated out code for automated convergence checks in runNimble.
    - Add more communication between parent process and samplers to pause one when the other is operating.

--- R/runNimble.R
+++ R/runNimble.R
@@ -21,13 +21,18 @@ runNimble <-
     directive.file <- paste0(dump.path, "/runNimbleDirective.txt")
     writeLines("GO", directive.file)
     if(automate.convergence.checks) {
+      for(cn in 1:nc) {
+        status.file <- paste0(dump.path, "/block",cn,"Status.txt")
+        writeLines("GO", status.file)
+      }
       check.log.file <- paste0(dump.path, "/Check_log.csv")
-      write.csv(data.frame(Model = character(), Check = numeric(), Time = character(),
-                           Min_waited = numeric(), Status = character()),
+      write.csv(data.frame(Model = character(), Check = numeric(),
+                           Time = character(), Status = character()),
                 check.log.file, row.names = FALSE)
     }
     save(list = c("model.path", "constants", "data", "inits", "parameters", "ni",
-                  "nt", "dump.path", "SamplerSourcePath"),
+                  "nt", "dump.path", "SamplerSourcePath", "check.freq",
+                  "automate.convergence.checks"),
          file = paste0(dump.path, "/NimbleObjects.RData"))
     #[Create R script for kicking off nimble run here]. Call it "ModRunScript.R"
     #___________________________________________________________________________#
@@ -43,17 +48,46 @@ runNimble <-
       "dump.file.path <- paste0(dump.path, '/mod_chn', chn, '_', i, '.RData')",
       "mod.comp <- runNimbleBlock(mod.lst = list(model, constants, data, inits, parameters, SamplerSourcePath = SamplerSourcePath),",
       "n.iter = ni, n.thin = nt, tmp.path = paste0(dump.path, '/tmp', chn), dump.file.path = dump.file.path)",
+      "if(automate.convergence.checks) {",
+        "status.file <- paste0(dump.path, '/block',cn,'Status.txt')",
+        "status.chain <- readLines(status.file)",
+        "i.stop <- check.freq",
+      "}",
       "directive <- readLines(paste0(dump.path, '/runNimbleDirective.txt'))",
       "while(directive != 'STOP') {",
-      "if(directive == 'GO') {",
-        "i <- i + 1",
-        "dump.file.path <- paste0(dump.path, '/mod_chn', chn, '_', i, '.RData')",
-        "mod.comp <- runNimbleBlock(comp.mcmc = mod.comp, n.iter = ni, tmp.path = paste0(dump.path, '/tmp', chn), dump.file.path = dump.file.path)",
-        "directive <- readLines(paste0(dump.path, '/runNimbleDirective.txt'))",
-      "} else if(directive == 'PAUSE') {",
-        "Sys.sleep(10)",
-        "directive <- readLines(paste0(dump.path, '/runNimbleDirective.txt'))",
-      "}",
+      "if(automate.convergence.checks) {",
+        "if(directive == 'GO' & status.chain == 'GO' & i < i.stop) {",
+          "i <- i + 1",
+          "dump.file.path <- paste0(dump.path, '/mod_chn', chn, '_', i, '.RData')",
+          "mod.comp <- runNimbleBlock(comp.mcmc = mod.comp, n.iter = ni, tmp.path = paste0(dump.path, '/tmp', chn), dump.file.path = dump.file.path)",
+          "directive <- readLines(paste0(dump.path, '/runNimbleDirective.txt'))",
+        "} else if(directive == 'GO' & status.chain == 'GO' & i == i.stop) {",
+          "writeLines('STOP', status.file)",
+          "status.chain <- readLines(status.file)",
+        "} else if(directive == 'PAUSE' | directive == 'GO' & status.chain == 'STOP') {",
+          "Sys.sleep(10)",
+          "directive <- readLines(paste0(dump.path, '/runNimbleDirective.txt'))",
+          "status.chain <- readLines(status.file)",
+        "} else if(status.chain == 'RESUME') {",
+          "i.stop <- i + check.freq",
+          "writeLines('GO', status.file)",
+          "status.chain <- readLines(status.file)",
+          "directive <- readLines(paste0(dump.path, '/runNimbleDirective.txt'))",
+        "} else {",
+          "error.message <- paste0('Undefined condition reached during automated convergence process on chain ', chn, '.')",
+          "writeLines(error.message, paste0(dump.path, '/block',cn,'Error.txt'))",
+        "}",
+      "} else {"
+        "if(directive == 'GO') {",
+          "i <- i + 1",
+          "dump.file.path <- paste0(dump.path, '/mod_chn', chn, '_', i, '.RData')",
+          "mod.comp <- runNimbleBlock(comp.mcmc = mod.comp, n.iter = ni, tmp.path = paste0(dump.path, '/tmp', chn), dump.file.path = dump.file.path)",
+          "directive <- readLines(paste0(dump.path, '/runNimbleDirective.txt'))",
+        "} else if(directive == 'PAUSE') {",
+          "Sys.sleep(10)",
+          "directive <- readLines(paste0(dump.path, '/runNimbleDirective.txt'))",
+        "}",
+      "}"
       "}"
     ),
     con = paste0(dump.path, "/ModRunScript.R"))
@@ -71,48 +105,25 @@ runNimble <-
     run.complete <- FALSE
     nchecks <- 1
     while(sum(str_detect(list.files(dump.path), "mod_chn")) < nc) {Sys.sleep(10)} # Wait until proc has written at least one file for each chain before going on.
-    if(nb > 1) {  # Also need to wait until we've passed burnin if burnin is absolute.
-      check.blocks <- countNimbleBlocks(read.path = dump.path, burnin = nb, ni.block = ni)
-      while(!nrow(check.blocks$m) > 0) {
-        Sys.sleep(10)
-        check.blocks <- countNimbleBlocks(read.path = dump.path, burnin = nb, ni.block = ni)
-      }
-    }
-    nblks.previous <- 0 # Will be updated as we go.
-    while(if(automate.convergence.checks) {
-        ifelse(is.null(max.tries), !mod.check.result,
-               !mod.check.result & nchecks < max.tries)
-      } else {
-        !run.complete
-      }) {
-      if(automate.convergence.checks) {
-        # Sys.sleep(check.freq) # Seems unnecessary now.
-        status <- "No new convergence information."
-      }
-      check.blocks <- countNimbleBlocks(read.path = dump.path, burnin = nb, ni.block = ni)
-      if(length(unique(check.blocks$m[,1])) > nc) stop("Error: Too many sampling blocks created. Code debug needed somewhere.")
-      if(automate.convergence.checks) {
-        while(length(unique(check.blocks$m[,1])) < nc |
-              (check.blocks$nblks * ni) <= 100) {
-          Sys.sleep(check.freq)
+    if(automate.convergence.checks) {
+      while(ifelse(is.null(max.tries), !mod.check.result,
+               !mod.check.result & nchecks < max.tries)) {
+        status.chains <- character(length = nc)
+        for(cn in 1:nc) status.chains[cn] <- readLines(paste0(dump.path, '/block',cn,'Status.txt'))
+        if(any(status.chains != "STOP")) {  # Wait for chains to finish sampling up to target amount.
+          Sys.sleep (60)
+          for(cn in 1:nc) status.chains[cn] <- readLines(paste0(dump.path, '/block',cn,'Status.txt'))
+        } else {
+
           check.blocks <- countNimbleBlocks(read.path = dump.path, burnin = nb, ni.block = ni)
-        }
-      } else {
-        while(length(unique(check.blocks$m[,1])) < nc) {
-          Sys.sleep(60)
-          check.blocks <- countNimbleBlocks(read.path = dump.path, burnin = nb, ni.block = ni)
-        }
-      }
-      write.csv(check.blocks$m, paste0(dump.path, "/m.csv"))
-      if(check.blocks$nblks > nblks.previous) {
-        nblks.previous <- nblks <- check.blocks$nblks
-        nb.now <- ifelse(nb<1, nb*ni*nblks, nb)
-        ni.now <- ni*nblks
-        
-        do.gather.check <- automate.convergence.checks | (!automate.convergence.checks &
-             ni.now >= max.samples.saved * nt)
-        if(do.gather.check) {
-          if(automate.convergence.checks) writeLines("PAUSE", paste0(dump.path, "runNimbleDirective.txt"))
+          if(length(unique(check.blocks$m[,1])) > nc) stop("Error: Too many sampling blocks created. Code debug needed somewhere.")
+          write.csv(check.blocks$m, paste0(dump.path, "/m.csv"))
+          
+          nblks <- check.blocks$nblks
+          nb.now <- ifelse(nb<1, nb*ni*nblks, nb)
+          ni.now <- ni*nblks
+          
+          writeLines("PAUSE", paste0(dump.path, "runNimbleDirective.txt"))
           mod.out <- suppressWarnings(
             gatherNimble(read.path = dump.path, directive.file = directive.file,
                          burnin = nb, ni.block = ni, base.thin = nt,
@@ -146,7 +157,7 @@ runNimble <-
                         " See 'Model_summary_PID",proc$get_pid(),
                         ".csv' for parameters missing Rhat or n.eff."))
           }
-          if(automate.convergence.checks & length(par.fuzzy.track) > 0) {
+          if(length(par.fuzzy.track) > 0) {
             Rht.fuzzy <- 1 # Putting in at least one value to avoid error later....
             if(!any(names(sumTab) == "Rhat")) {
               # proc$kill_tree()
@@ -163,14 +174,9 @@ runNimble <-
             prp.fuzzy.not.coverged <- (sum(round(Rht.fuzzy, digits = 1) > Rht.required, na.rm = T) /
                                          length(Rht.fuzzy))
           }
-          
           mod <- list(mcmcOutput = mod.out$out, summary = sumTab, mcmc.info = mcmc.info)
           if(sav.model) R.utils::saveObject(mod, mod.nam)
           if(rtrn.model) assign("mod", mod.nam, envir = .GlobalEnv)
-          if(!automate.convergence.checks) run.complete <- TRUE
-        } # Close if(do.gather.check) loop
-        
-        if(automate.convergence.checks) {
           if(!mod.check.result & length(par.fuzzy.track) == 0) {
             status <- paste0("Max Rhat = ", max(sumTab.focal$Rhat), " and min neff = ",
                              min(sumTab.focal$n.eff))
@@ -188,33 +194,84 @@ runNimble <-
                              ", and proportion fuzzy parameters not converged = ",
                              round(prp.fuzzy.not.coverged, digits = 2))
           }
+          check.log <- read.csv(check.log.file, colClasses = c("character",
+                                                               "numeric",
+                                                               "character",
+                                                               "character"))
+          check.log <- check.log %>% bind_rows(
+            data.frame(Model = mod.nam, Check = nchecks, Time = as.character(Sys.time()),
+                       Status = status)
+          )
+          write.csv(check.log, check.log.file, row.names = FALSE)
+          nchecks <- nchecks + 1
+          
+          if(ifelse(is.null(max.tries), !mod.check.result,
+                    !mod.check.result & nchecks < max.tries)) {
+            for(cn in 1:nc) writeLines("GO", paste0(dump.path, '/block',cn,'Status.txt'))
+            writeLines("GO", paste0(dump.path, "runNimbleDirective.txt"))
+            suppressWarnings(rm(mod, mod.out, mod.check, sumTab, sumTab.focal))
+            gc()
+          }
+        } # Close if(any(status.chains != "STOP")) {} else {}
+      } # Close primary while loop, i.e., while(ifelse(is.null(max.tries), ..., ...))
+      writeLines("STOP", directive.file)
+      if(!mod.check.result) {
+        warn.message <- paste0("Rhat did not decrease after ", nchecks,
+                               " checks. Model abandoned before reaching convergence targets.")
+        mod <- list(mcmcOutput = mod.out$out, summary = sumTab, mcmc.info = mcmc.info,
+                    warning = warn.message)
+        if(sav.model) R.utils::saveObject(mod, mod.nam)
+        if(rtrn.model) assign("mod", mod.nam, envir = .GlobalEnv)
+      }
+      if(delete.blocks) unlink(dump.path, recursive = TRUE)
+      gc(verbose = FALSE)
+    } else {
+      if(nb > 1) {  # Also need to wait until we've passed burnin if burnin is absolute.
+        check.blocks <- countNimbleBlocks(read.path = dump.path, burnin = nb, ni.block = ni)
+        while(!nrow(check.blocks$m) > 0) {
+          Sys.sleep(10)
+          check.blocks <- countNimbleBlocks(read.path = dump.path, burnin = nb, ni.block = ni)
         }
       }
-      if(automate.convergence.checks) {
-        check.log <- read.csv(check.log.file, colClasses = c("character", "numeric", "character",
-                                                             "numeric", "character"))
-        check.log <- check.log %>% bind_rows(
-          data.frame(Model = mod.nam, Check = nchecks, Time = as.character(Sys.time()),
-                     Min_waited = (nchecks * check.freq / 60), Status = status)
-        )
-        write.csv(check.log, check.log.file, row.names = FALSE)
-      }
-      nchecks <- nchecks + 1
-      if(automate.convergence.checks & !mod.check.result) {
-        writeLines("GO", paste0(dump.path, "runNimbleDirective.txt"))
-        Sys.sleep(check.freq)
-      }
-    } # Close primary while loop, i.e., while(if(automate.convergence.checks) {...} else {...})
-    # proc$kill_tree()
-    writeLines("STOP", directive.file)
-    if(automate.convergence.checks & !mod.check.result) {
-      warn.message <- paste0("Rhat did not decrease after ", nchecks,
-                            " checks. Model abandoned before reaching convergence targets.")
-      mod <- list(mcmcOutput = mod.out$out, summary = sumTab, mcmc.info = mcmc.info,
-                  warning = warn.message)
-      if(sav.model) R.utils::saveObject(mod, mod.nam)
-      if(rtrn.model) assign("mod", mod.nam, envir = .GlobalEnv)
+      nblks.previous <- 0 # Will be updated as we go.
+      while(!run.complete) {
+        check.blocks <- countNimbleBlocks(read.path = dump.path, burnin = nb, ni.block = ni)
+        if(length(unique(check.blocks$m[,1])) > nc) stop("Error: Too many sampling blocks created. Code debug needed somewhere.")
+        while(length(unique(check.blocks$m[,1])) < nc) {
+          Sys.sleep(60)
+          check.blocks <- countNimbleBlocks(read.path = dump.path, burnin = nb, ni.block = ni)
+        }
+        write.csv(check.blocks$m, paste0(dump.path, "/m.csv"))
+        nblks <- check.blocks$nblks
+        nb.now <- ifelse(nb<1, nb*ni*nblks, nb)
+        ni.now <- ni*nblks
+
+        do.gather.check <- (ni.now - nb.now) >= max.samples.saved * nt
+        if(do.gather.check) {
+          writeLines("STOP", directive.file)
+          mod.out <- suppressWarnings(
+            gatherNimble(read.path = dump.path, directive.file = directive.file,
+                         burnin = nb, ni.block = ni, base.thin = nt,
+                         max.samples.saved = max.samples.saved)
+          )
+          if(!is.null(par.ignore)) mod.out.subset <- mcmcOutputSubset(mod.out$out, par.drop = par.ignore)
+          mcmc.info <- c(nchains = nc, niterations = ni.now,
+                         burnin = nb.now, nthin = nt)
+          if(!is.null(par.ignore)) {
+            sumTab <- summary(mod.out.subset, MCEpc = F, Rhat = T, n.eff = T, f = T, overlap0 = T, verbose = FALSE)
+          } else {
+            sumTab <- summary(mod.out$out, MCEpc = F, Rhat = T, n.eff = T, f = T, overlap0 = T, verbose = FALSE)
+          }
+          mod <- list(mcmcOutput = mod.out$out, summary = sumTab, mcmc.info = mcmc.info)
+          if(sav.model) R.utils::saveObject(mod, mod.nam)
+          if(rtrn.model) assign("mod", mod.nam, envir = .GlobalEnv)
+          run.complete <- TRUE
+        } else {
+          Sys.sleep(300)
+        } # Close if(do.gather.check) loop
+        nchecks <- nchecks + 1
+      } # Close while(!run.complete)
+      if(delete.blocks) unlink(dump.path, recursive = TRUE)
+      gc(verbose = FALSE)
     }
-    if(delete.blocks) unlink(dump.path, recursive = TRUE)
-    gc(verbose = FALSE)
   }
