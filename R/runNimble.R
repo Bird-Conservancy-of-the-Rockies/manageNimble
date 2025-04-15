5ad7d0f2742e98f95953e2825ea6ec7afba2b3f1
Quresh Latif <qureshlatif@yahoo.com>
2025-04-15 08:05:44 -0600

    no need to check if dump path exists anymore
    We're deleting it, so why do we need to check if it exists?!

--- R/runNimble.R
+++ R/runNimble.R
@@ -18,7 +18,7 @@ runNimble <-
     require(coda)
     require(mcmcOutput)
     if(dir.exists(dump.path)) unlink(dump.path, recursive = TRUE)
-    if(!dir.exists(dump.path)) dir.create(dump.path)
+    dir.create(dump.path)
     directive.file <- paste0(dump.path, "/runNimbleDirective.txt")
     writeLines("GO", directive.file)
     if(automate.convergence.checks) {
